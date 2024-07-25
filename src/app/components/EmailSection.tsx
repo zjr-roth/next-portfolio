d3a573e5ff591c78846a097888b9a3f8aa63fbd6
Zach Roth <151420999+zjr-roth@users.noreply.github.com>
2024-07-25 15:18:05 -0400

    updates

--- src/app/components/EmailSection.tsx
+++ src/app/components/EmailSection.tsx
@@ -1,6 +1,50 @@
-import {FC} from 'react'
+"use client"
+import { FC, SetStateAction, useState } from 'react'
+import { supabase } from '../lib/database/supabaseClient';
+
 
 const EmailSection: FC = () => {
+    const [name, setName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [phone, setPhone] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
+
+    const handleName = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setName(event.target.value)
+    }
+
+    const handleEmail = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setEmail(event.target.value)
+    }
+
+    const handlePhone = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setPhone(event.target.value)
+    }
+
+    const handleMessage = (event: { target: { value: SetStateAction<string>; }; }) => {
+        setMessage(event.target.value)
+    }
+
+    const postData = async (event: any) => {
+        event.preventDefault();
+        console.log("Sending data to Supabase:", { email, name, phone, message})
+        
+        const { data, error } = await supabase
+        .from("FormData")
+        .insert([{ email, name, phone, message }])
+
+        if (error) {
+            console.error("Submission Failed", error);
+        } else {
+            console.log("Submitted Successfully", data);
+            setName("");
+            setEmail("");
+            setPhone("");
+            setMessage("");
+        }
+    }
+
+
     return (
         <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
             <div>
@@ -15,22 +59,22 @@ const EmailSection: FC = () => {
                 <a href="#resume"><button type="button" className="px-1 py-1 bg-gradient-to-br from-orange-700 via-orange-500 to-orange-800 text-white w-full sm:w-1/2 rounded-full transition h-8 hover:opacity-75"><span className="block bg-[#121212] hover:bg-slate-800 rounded-full">Download CV</span></button></a>
             </div>
             <div>
-                <form className="flex flex-col gap-4">
+                <form onSubmit={postData} className="flex flex-col gap-4">
                     <div className="mb-6">
                         <label htmlFor="name"className="text-white block mb-2 text-sm font-medium">Full Name</label>
-                        <input name="name" id="name" type="text" required className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="Harry Potter"/>
+                        <input onChange={handleName} name="name" id="name" type="text" required className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="Harry Potter"/>
                     </div>
                     <div className="mb-6"> 
                         <label htmlFor="email"className="text-white block mb-2 text-sm font-medium">Email</label>
-                        <input name="email" id="email" type="email" required className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="Harry@Hogwarts.edu"/>
+                        <input onChange={handleEmail} name="email" id="email" type="email" required className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="Harry@Hogwarts.edu"/>
                     </div>
                     <div className="mb-6">
                         <label htmlFor="phone"className="text-white block mb-2 text-sm font-medium">Phone</label>
-                        <input name="phone" id="phone" type="tel" className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="(888)-888-8888"/>
+                        <input onChange={handlePhone} name="phone" id="phone" type="tel" className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="(888)-888-8888"/>
                     </div>
                     <div className="mb-6">
                         <label htmlFor="message"className="text-white block mb-2 text-sm font-medium">Message</label>
-                        <textarea name="message" id="message" className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="Hi Harry! :)"/>
+                        <textarea onChange={handleMessage} name="message" id="message" className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" placeholder="Hey! "/>
                     </div>
                     <button className="bg-gradient-to-br from-orange-700 via-orange-500 to-orange-800 hover:opacity-75 text-white font-medium py-2.5 px-5 rounded-lg w-full">Submit</button>
                 </form>
--- src/app/lib/database/supabaseClient.tsx
+++ src/app/lib/database/supabaseClient.tsx
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl: any = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey: any = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient("https://bxtqczklwnbyktqhyszg.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4dHFjemtsd25ieWt0cWh5c3pnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5MjI3NzMsImV4cCI6MjAzNzQ5ODc3M30.HYtPlQwngZM8X97fDcexRytKKBZj4p5wXf1v9dSSLy8")
 
